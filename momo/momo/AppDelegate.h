e0ae75a660d0ffc52952b898feec91ae97b99976
wcp <wcpneu@163.com>
2016-06-26 15:31:41 +0800

    初始化项目

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by wangchunpeng on 16/6/26.
+//  Copyright © 2016年 wangchunpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ momo/momo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  momo
+//
+//  Created by wangchunpeng on 16/6/26.
+//  Copyright © 2016年 wangchunpeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
